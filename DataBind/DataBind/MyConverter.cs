54cb168b6e09903d8c7281b98f7985c1d8203667
hoge-jager <zhong39@live.com>
2018-01-10 10:52:32 +0800

    这个是WPF的绑定的例子,仅仅供学习使用

---
+++ DataBind/DataBind/MyConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DataBind
+{
+    public class MyConverter : IValueConverter
+    {
+        //转换器
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //throw new NotImplementedException();
+            var date = (DateTime)value;
+            //判断类型
+            switch (targetType.Name)
+            {
+                case "String":
+                    return date.ToString("d",culture);
+                case "Brush":
+                    return Brushes.Blue;
+                default:
+                    return value;
+            }
+        }
+
+        //public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        //{
+        //    return null;
+        //}
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => null;
+
+    }
+}
---
+++ DataBind/DataBind/MyData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBind
+{
+    public class MyData : INotifyPropertyChanged
+    {
+        private DateTime TheDates;
+        //声明事件
+        public event PropertyChangedEventHandler PropertyChanged;
+        public MyData()
+        {
+            TheDates = DateTime.Now;
+        }
+        //把现在的时间拿到
+        public DateTime TheDate
+        {
+            get { return TheDates; }
+            set {
+                TheDates = value;
+                OnPropertyChanged("");
+            }
+        }
+        private void OnPropertyChanged(string info)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(info));
+        }
+    }
+}
